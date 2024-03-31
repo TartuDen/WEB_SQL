d229d3060344a213b1abea959d8f30a3c36151c1
dverves <denver1033@gmail.com>
2024-03-31 12:34:29 +0300

    combine countries and flags games

--- countries/index.js
+++ countries/index.js
@@ -8,7 +8,8 @@ const port = 8080;
 const app = express();
 const apiBasicUrl = "http://localhost:8081/api/v01";
 let globalAnsw = null;
-let nextSeq = [];
+let nextSeqCountries = [];
+let nextSeqFlags = [];
 let score = 0;
 
 
@@ -54,11 +55,41 @@ async function checkVariants(nextQ, vars) {
   
     return vars;
   }
+
+  async function checkVariantsF(nextQ, vars) {
+    // Check if any of the existing variants have the same capital as the next question
+    const isCapitalPresent = vars.some(variant => variant.name === nextQ.name);
+  
+    if (!isCapitalPresent) {
+      // If not present, replace a random variant (excluding the first one)
+      const randomIndex = Math.floor(Math.random() * (vars.length - 1)) + 1; // Exclude first element (index 0)
+      vars[randomIndex] = nextQ;
+    }
+  
+    return vars;
+  }
   
  
-app.post("/answ",(req,res)=>{
+app.post("/answCountry",(req,res)=>{
     let clientAnsw = req.body.answer;
     let correctAnsw = req.body.correctAnsw;
+    if(correctAnsw === clientAnsw){
+        console.log("clinetAnsw:",clientAnsw),
+        console.log("correctAnsw:",correctAnsw);
+        globalAnsw = true;
+        score=score+1
+    }else{
+        globalAnsw = false;
+        score=0;
+
+    }
+    res.status(200).redirect("/countries")
+})
+
+app.post("/answFlags",(req,res)=>{
+    let clientAnsw = req.body.answer;
+    let correctAnsw = req.body.correctAnsw;
+    
     if(correctAnsw === clientAnsw){
         globalAnsw = true;
         score=score+1
@@ -67,26 +98,43 @@ app.post("/answ",(req,res)=>{
         score=0;
 
     }
-    res.status(200).redirect("/")
+    res.status(200).redirect("/flags")
 })
 
-app.get("/", async (req,res)=>{
+app.get("/", async(req,res)=>{
+    res.status(200).render("index.ejs",{score});
+})
+
+app.post("/", async(req,res)=>{
+        res.status(200).redirect(`/${req.body.gameType}`)
+
+})
+
+app.get("/countries", async (req,res)=>{
     let localAnsw = globalAnsw;
     globalAnsw = null
-    nextSeq = await getNextQuestion();
-    console.log("*****************");
+    nextSeqCountries = await getNextQuestion();
+    nextSeqFlags = await getNextFlag();
+    console.log(nextSeqCountries);
+    console.log(nextSeqFlags);
+    let nextQuestionC = await nextSeqCountries.nextQ[0];
+    let nextVariants = await nextSeqCountries.nextQ.slice(1,5);
 
-    let nextQuestion = await nextSeq.nextQ[0];
-    let nextVariants = await nextSeq.nextQ.slice(1,5);
-
-    nextVariants = await checkVariants(nextQuestion, nextVariants);
+    nextVariants = await checkVariants(nextQuestionC, nextVariants);
   
-    // console.log(nextVariants);
-    res.status(200).render("index.ejs",{nextQuestion, nextVariants, localAnsw, score})
+    res.status(200).render("index.ejs",{nextQuestionC, nextVariants, localAnsw, score})
 })
 
 app.get("/flags",async(req,res)=>{
-    
+    let localAnsw = globalAnsw;
+    globalAnsw = null
+    nextSeqFlags = await getNextFlag();
+    let nextQuestionF = await nextSeqFlags.nextQ[0]
+    let nextVariantsF = await nextSeqFlags.nextQ.slice(1,5);
+    nextVariantsF = await checkVariantsF(nextQuestionF, nextVariantsF);
+    console.log(nextQuestionF);
+    console.log(nextVariantsF);
+    res.status(200).render("index copy.ejs",{nextQuestionF, nextVariantsF, localAnsw, score})
 })
 
 
