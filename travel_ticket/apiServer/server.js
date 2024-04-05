f650ec32d8842bf535291644ec4bec85aca5bcee
dverves <denver1033@gmail.com>
2024-04-05 23:49:54 +0300

    connect pg db to apiServer

--- travel_ticket/apiServer/server.js
+++ travel_ticket/apiServer/server.js
@@ -1,17 +1,44 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import ejs from 'ejs';
+import pg from 'pg';
+
+
+  
 
 
 const port  = 8081;
 const app = express();
-let data = ["FR","ES"];
+let data = [];
+let dataFromDB;
 
+const pool = new pg.Pool({
+    user: 'dverves',
+    host: 'localhost',
+    database: 'postgres',
+    password: '123',
+    port: 5432,
+  });
+  pool.query('SELECT * FROM visited_countries', (err, res) => {
+    if (err) {
+      console.error(err);
+    } else {
+        dataFromDB = res.rows
+      console.log(dataFromDB);
+    }
+    // The connection is automatically released back to the pool
+  });
+
+  function extractCountryCodes(dataFromDB) {
+    return dataFromDB.map(item => item.country_code);
+  }
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.get("/api/v01",async (req,res)=>{
+    data = extractCountryCodes(dataFromDB);
+    console.log(data)
     res.status(200).json(data);
 })
 
