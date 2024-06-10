8a04956b9981b87eb6cf479b4f1b0391d3d7e825
TartuDen <denver1033@gmail.com>
2024-06-10 20:09:33 +0300

    made my timer

--- React_web/react_hooks/src/App.jsx
+++ React_web/react_hooks/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // let time="0";
+  const [time, setTime] = React.useState(getCurrentTime);
+
+  function getCurrentTime() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    let curTime= `${hours}:${minutes}:${seconds}`;
+    return curTime;
+  }
+
+  function changeTime(){
+    setTime(getCurrentTime())
+  }
+
+  setInterval(changeTime,1000)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container centered">
+    <h1 className="text-center">{time}</h1>
+    <p>_____</p>
+    {/* <button style={{marginBottom: "10px"}} type="button" className="btn btn-primary" onClick={changeTime}>check time</button> */}
+
+  </div>
+
+  );
 }
 
-export default App
+export default App;
