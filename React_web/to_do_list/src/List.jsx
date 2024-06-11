b8123127f25bf345f10ee4f9088b4cce376011be
TartuDen <denver1033@gmail.com>
2024-06-12 00:22:10 +0300

    start next task

--- React_web/to_do_list/src/List.jsx
+++ React_web/to_do_list/src/List.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function List(props){
     let taskList = props.tasksToAdd;
 
+    const [checkTask, setChangeTask]=useState(false);
+    function checkTaskFunc(event){
+        console.log(event.target.value)
+    }
+
     return(
         <ul id="todo-list" className="list-unstyled">
-            {taskList && taskList.map((task,idx)=><li key={idx} >{idx+1}) {task}</li>)}
+            {taskList && taskList.map((task,idx)=><li key={idx} onClick={checkTaskFunc} >{idx+1}) {task}</li>)}
       </ul>
     )
 }
