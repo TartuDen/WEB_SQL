e128d23c87f121c8f23cf49a5fb19f7a88cf86b1
Pomogadryg <denver1033@gmail.com>
2024-06-14 16:40:29 +0300

    set up BRG on reacto. preparing files and structure

---
+++ React_web/BR_gen_react/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './css/App.css'
+import Header from './components/header'
+
+function App() {
+
+
+
+  return (<div>
+    <Header />
+  </div>
+
+  )
+}
+
+export default App
